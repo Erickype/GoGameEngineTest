9c87c739c9e85611282f4fc364e64bb187ee3786
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-03 15:51:16 -0500

    Example layer

---
+++ Layers/exampleL.go
@@ -0,0 +1,19 @@
+package Layers
+
+import (
+	"github.com/Erickype/GoGameEngine/Common"
+	core "github.com/Erickype/GoGameEngine/Core"
+	"github.com/Erickype/GoGameEngine/Events"
+)
+
+type ExampleLayer struct {
+	core.Layer
+}
+
+func (e *ExampleLayer) OnUpdate() {
+	Common.ClientLogger.Info(e.GetName(), "Layer: Update")
+}
+
+func (e *ExampleLayer) OnEvent(event *Events.IEvent) {
+	Common.ClientLogger.Trace(event)
+}
