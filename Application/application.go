6676d4ba7f41502db28a5267310bd9bb8bf74522
Erickype <67160282+Erickype@users.noreply.github.com>
2023-03-03 15:51:05 -0500

    Application inherit the game engine application

--- Application/application.go
+++ Application/application.go
@@ -2,6 +2,14 @@ package Application
 
 import core "github.com/Erickype/GoGameEngine/Core"
 
-func CreateApplication() {
-	core.CreateApplication()
+type _ interface {
+	Construct(layer *core.ILayer)
+}
+
+type Application struct {
+	*core.Application
+}
+
+func (a *Application) Construct(layer *core.ILayer) {
+	core.CreateApplication(layer)
 }
